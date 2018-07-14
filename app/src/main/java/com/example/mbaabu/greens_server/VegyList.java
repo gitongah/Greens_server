3df83d6e44411749b5f1af1f80dd86638e205773
gitongah <mbaabugitonga@gmail.com>
2018-07-14 09:03:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mbaabu/greens_server/VegyList.java
@@ -0,0 +1,13 @@
+package com.example.mbaabu.greens_server;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VegyList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vegy_list);
+    }
+}
